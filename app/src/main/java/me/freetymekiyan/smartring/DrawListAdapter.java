8bcc23154c441c95ea6f14660365ee31b00a46b8
Kiyan <freetymesunkiyan@gmail.com>
2015-01-16 14:02:31 -0500

    Initialized commit

---
+++ app/src/main/java/me/freetymekiyan/smartring/DrawListAdapter.java
@@ -0,0 +1,103 @@
+package me.freetymekiyan.smartring;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by Kiyan on 1/12/15.
+ */
+public class DrawListAdapter extends RecyclerView.Adapter<DrawListAdapter.ViewHolder> {
+
+    private static final int TYPE_HEADER = 0;
+
+    private static final int TYPE_ITEM = 1;
+
+    String[] titles;
+
+    int[] icons;
+
+    private String name;
+
+    private int profile;
+
+    private String email;
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        int holderId;
+
+        TextView tvTitle;
+
+        ImageView ivIcon;
+
+        ImageView ivProfile;
+
+        TextView tvName;
+
+        TextView tvEmail;
+
+        ViewHolder(View itemView, int viewType) {
+            super(itemView);
+            if (viewType == TYPE_ITEM) {
+                tvTitle = (TextView) itemView.findViewById(R.id.rowText);
+                ivIcon = (ImageView) itemView.findViewById(R.id.rowIcon);
+                holderId = TYPE_ITEM;
+            } else {
+                tvName = (TextView) itemView.findViewById(R.id.name);
+                tvEmail = (TextView) itemView.findViewById(R.id.email);
+                ivProfile = (ImageView) itemView.findViewById(R.id.circleView);
+                holderId = TYPE_HEADER;
+            }
+        }
+    }
+
+    public DrawListAdapter(String[] titles, int[] icons, String name, int profile, String email) {
+        this.titles = titles;
+        this.icons = icons;
+        this.name = name;
+        this.profile = profile;
+        this.email = email;
+    }
+
+    @Override
+    public DrawListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType == TYPE_ITEM) {
+            View v = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.list_item_drawer, parent, false);
+            ViewHolder vh = new ViewHolder(v, viewType);
+            return vh;
+        } else if (viewType == TYPE_HEADER) {
+            View v = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.list_header_drawer, parent, false);
+            ViewHolder header = new ViewHolder(v, viewType);
+            return header;
+        }
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(DrawListAdapter.ViewHolder holder, int position) {
+        if (holder.holderId == TYPE_ITEM) {
+            holder.tvTitle.setText(titles[position - 1]);
+            holder.ivIcon.setImageResource(icons[position - 1]);
+        } else {
+            holder.ivProfile.setImageResource(profile);
+            holder.tvName.setText(name);
+            holder.tvEmail.setText(email);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return titles.length + 1;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position == 0 ? TYPE_HEADER : TYPE_ITEM;
+    }
+}
---
+++ app/src/main/java/me/freetymekiyan/smartring/MainActivity.java
@@ -0,0 +1,86 @@
+package me.freetymekiyan.smartring;
+
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private Toolbar toolbar;
+
+    String name = "Yang Liu";
+
+    String email = "freetymesunkiyan@gmail.com";
+
+    int[] icons = {R.drawable.home, R.drawable.events, R.drawable.mail, R.drawable.shop,
+            R.drawable.travel};
+
+    int profile = R.drawable.photo;
+
+    RecyclerView mRcView;
+
+    RecyclerView.Adapter mAdapter;
+
+    RecyclerView.LayoutManager mLayoutManager;
+
+    DrawerLayout Drawer;
+
+    ActionBarDrawerToggle mDrawerToggle;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+
+        mRcView = (RecyclerView) findViewById(R.id.recycle_view);
+        mRcView.setHasFixedSize(true);
+        mAdapter = new DrawListAdapter(getResources().getStringArray(R.array.titles), icons, name,
+                profile, email);
+        mRcView.setAdapter(mAdapter);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRcView.setLayoutManager(mLayoutManager);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer);
+        mDrawerToggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.open_drawer,
+                R.string.close_drawer) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+
+        drawer.setDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
