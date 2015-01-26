a7c64900572888c87d30537a3ef8c0786e81734f
Kiyan <freetymesunkiyan@gmail.com>
2015-01-26 16:11:43 -0500

    Added settings activity

---
+++ app/src/main/java/me/freetymekiyan/smartring/SettingsActivity.java
@@ -0,0 +1,26 @@
+package me.freetymekiyan.smartring;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class SettingsActivity extends ActionBarActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pref_with_actionbar);
+        Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        getFragmentManager().beginTransaction()
+                .replace(R.id.content_frame, new MyPreferenceFragment()).commit();
+    }
+}
