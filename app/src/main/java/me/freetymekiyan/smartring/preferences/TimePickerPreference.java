5887386ce56e5c0721c39c174c35b9f3c2ff4345
Kiyan <freetymesunkiyan@gmail.com>
2015-02-02 17:26:19 -0500

    Reformat time picker dialog

--- app/src/main/java/me/freetymekiyan/smartring/preferences/TimePickerPreference.java
+++ app/src/main/java/me/freetymekiyan/smartring/preferences/TimePickerPreference.java
@@ -1,11 +1,5 @@
 package me.freetymekiyan.smartring.preferences;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.preference.DialogPreference;
@@ -13,13 +7,21 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.widget.TimePicker;
 
-// Based on http://stackoverflow.com/a/7484289/922168
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class TimePickerPreference extends DialogPreference {
+
     private int mHour = 0;
+
     private int mMinute = 0;
+
     private TimePicker picker = null;
-	private final String DEFAULT_VALUE = "00:00";
+
+    private final String DEFAULT_VALUE = "00:00";
 
     public static int getHour(String time) {
         String[] pieces = time.split(":");
@@ -44,23 +46,23 @@ public class TimePickerPreference extends DialogPreference {
         setPositiveButtonText("Set");
         setNegativeButtonText("Cancel");
     }
-    
+
     public void setTime(int hour, int minute) {
-    	mHour = hour;
-    	mMinute = minute;
-    	String time = toTime(mHour, mMinute);
-    	persistString(time);
+        mHour = hour;
+        mMinute = minute;
+        String time = toTime(mHour, mMinute);
+        persistString(time);
         notifyDependencyChange(shouldDisableDependents());
         notifyChanged();
     }
-    
+
     public String toTime(int hour, int minute) {
-    	return String.valueOf(hour) + ":" + String.valueOf(minute);
+        return String.valueOf(hour) + ":" + String.valueOf(minute);
     }
-    
+
     public void updateSummary() {
-    	String time = String.valueOf(mHour) + ":" + String.valueOf(mMinute);
-    	setSummary(time24to12(time));
+        String time = String.valueOf(mHour) + ":" + String.valueOf(mMinute);
+        setSummary(time24to12(time));
     }
 
     @Override
@@ -83,14 +85,14 @@ public class TimePickerPreference extends DialogPreference {
         if (positiveResult) {
             int currHour = picker.getCurrentHour();
             int currMinute = picker.getCurrentMinute();
-        	
-        	if (!callChangeListener(toTime(currHour, currMinute))) {
-        		return;
-        	}
 
-        	// persist
-        	setTime(currHour, currMinute);
-        	updateSummary();
+            if (!callChangeListener(toTime(currHour, currMinute))) {
+                return;
+            }
+
+            // persist
+            setTime(currHour, currMinute);
+            updateSummary();
         }
     }
 
@@ -106,12 +108,10 @@ public class TimePickerPreference extends DialogPreference {
         if (restorePersistedValue) {
             if (defaultValue == null) {
                 time = getPersistedString(DEFAULT_VALUE);
-            }
-            else {
+            } else {
                 time = getPersistedString(DEFAULT_VALUE);
             }
-        }
-        else {
+        } else {
             time = defaultValue.toString();
         }
 
@@ -121,23 +121,23 @@ public class TimePickerPreference extends DialogPreference {
         setTime(currHour, currMinute);
         updateSummary();
     }
-    
+
     public static Date toDate(String inTime) {
-    	try {
-	    	DateFormat inTimeFormat = new SimpleDateFormat("HH:mm", Locale.US);
-	    	return inTimeFormat.parse(inTime);
-    	} catch(ParseException e) {
-    		return null;
-    	}
+        try {
+            DateFormat inTimeFormat = new SimpleDateFormat("HH:mm", Locale.US);
+            return inTimeFormat.parse(inTime);
+        } catch (ParseException e) {
+            return null;
+        }
     }
-    
+
     public static String time24to12(String inTime) {
-    	Date inDate = toDate(inTime);
-    	if(inDate != null) {
-			DateFormat outTimeFormat = new SimpleDateFormat("hh:mm a", Locale.US);
-			return outTimeFormat.format(inDate);
-    	} else {
-    		return inTime;
-    	}
+        Date inDate = toDate(inTime);
+        if (inDate != null) {
+            DateFormat outTimeFormat = new SimpleDateFormat("hh:mm a", Locale.US);
+            return outTimeFormat.format(inDate);
+        } else {
+            return inTime;
+        }
     }
 }
