0051591a673488b8a13486bd7484f3b760614012
Kiyan <freetymesunkiyan@gmail.com>
2015-02-06 00:09:40 -0500

    Added event bus

---
+++ app/src/main/java/me/freetymekiyan/smartring/controllers/MeasureEvent.java
@@ -0,0 +1,12 @@
+package me.freetymekiyan.smartring.controllers;
+
+/**
+ * Created by Kiyan on 2/5/15.
+ */
+public class MeasureEvent {
+    public final String message;
+
+    public MeasureEvent(String message) {
+        this.message = message;
+    }
+}
