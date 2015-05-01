076a9ede1cdd68b92810bb85dd1a04f34779d1ab
Kiyan <freetymesunkiyan@gmail.com>
2015-05-01 01:18:30 -0400

    Finished report fragment

--- app/src/main/java/me/freetymekiyan/smartring/views/ReportFragment.java
+++ app/src/main/java/me/freetymekiyan/smartring/views/ReportFragment.java
@@ -1,16 +1,28 @@
 package me.freetymekiyan.smartring.views;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
+import de.greenrobot.event.EventBus;
 import me.freetymekiyan.smartring.R;
+import me.freetymekiyan.smartring.utils.Utils;
+import me.freetymekiyan.smartring.controllers.PageChangedEvent;
 
 public class ReportFragment extends Fragment {
+
+    public ReportFragment() {
+        // Required empty public constructor
+    }
 
     public static ReportFragment newInstance(String title) {
         ReportFragment fragment = new ReportFragment();
@@ -18,10 +30,6 @@ public class ReportFragment extends Fragment {
         args.putString(MainActivity.KEY_TITLE, title);
         fragment.setArguments(args);
         return fragment;
-    }
-
-    public ReportFragment() {
-        // Required empty public constructor
     }
 
     @Override
@@ -33,11 +41,41 @@ public class ReportFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_report, container, false);
-        final TableLayout tlReport = (TableLayout) view.findViewById(R.id.tl_report);
+        final SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        final TextView tvUsername = (TextView) view.findViewById(R.id.tv_username);
+        String username = sp.getString(getString(R.string.key_name), "");
+        tvUsername.setText(username.isEmpty() ? getString(R.string.ph_name) : username);
+        final String age = sp.getString(getString(R.string.key_age), "");
+        final Button btnGenerate = (Button) view.findViewById(R.id.btn_generate);
+        btnGenerate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // open settings page
+                EventBus.getDefault().post(new PageChangedEvent(MainActivity.SETTINGS_FRAGMENT));
+            }
+        });
+        if (!sp.contains(getString(R.string.key_gender)) || age.isEmpty()) {
+            return view;
+        }
 
-//        TableRow tr = new TableRow(getActivity());
-//        tlReport.addView(tr);
+        final boolean gender = sp.getBoolean(getString(R.string.key_gender), false);
+        float mxr = Utils.getUpperLimitValue(getActivity());
+        final TableLayout tlReport = (TableLayout) view.findViewById(R.id.tl_report);
+        for (int i = 1; i < tlReport.getChildCount(); i++) {
+            final TableRow row = (TableRow) tlReport.getChildAt(i);
+            final TextView figure = (TextView) row.getChildAt(2);
+            figure.setText(getFigureWithPosition(i, mxr));
+        }
+
+        btnGenerate.setVisibility(View.GONE);
         return view;
+    }
+
+    private String getFigureWithPosition(int i, float mxr) {
+        i--;
+        int lowerLimit = (int) ((0.5 + 0.1 * i) * mxr);
+        int upperLimit = lowerLimit + (int) (0.1 * mxr);
+        return lowerLimit + "-" + upperLimit;
     }
 
     @Override
